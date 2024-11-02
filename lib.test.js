a8d289b2a751521f82022e169a82d17674d6e977
jangsh <shshshsh77710@gmail.com>
2024-11-02 19:40:25 +0900

    test final

--- lib.test.js
+++ lib.test.js
@@ -11,18 +11,18 @@ test("avg([-5, 5]) should be 0", () => {
 
 
 test("prime([2]) should be true", () => {
-    expect(prime(2)).toBe(true);  // 소수
+    expect(lib.prime([2])).toBe(true);  // 소수
 })
 
 test("prime([63]) should be false", () => {
-    expect(prime(63)).toBe(false);  // 소수x
+    expect(lib.prime([63])).toBe(false);  // 소수x
 })
 
 
 test("factorial([0]) should be 1", () => {
-    expect(factorial(0)).toBe(1);   // 0! = 1
+    expect(lib.factorial([0])).toBe(1);   // 0! = 1
 });
 
 test("factorial([4]) should be 24", () => {
-    expect(factorial(4)).toBe(24); // 4! = 24
+    expect(lib.factorial([4])).toBe(24); // 4! = 24
 });
